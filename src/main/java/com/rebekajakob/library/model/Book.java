575db865412bccc30f94f8f40c4400fa3b19de41
rebekajakob <rebusacka@gmail.com>
2023-01-03 14:36:42 +0100

    add id to Book

--- src/main/java/com/rebekajakob/library/model/Book.java
+++ src/main/java/com/rebekajakob/library/model/Book.java
@@ -4,10 +4,12 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.UUID;
 
 @Builder
 @Getter
 public class Book {
+    private UUID id;
     private String title;
     private Author author;
     private String description;
