97ce1e78c16e7b15a69b48d33cb7035e4e30f1e4
rebekajakob <rebusacka@gmail.com>
2023-01-03 15:00:39 +0100

    create BookRepository and LibraryUserRepository interfaces

---
+++ src/main/java/com/rebekajakob/library/repository/LibraryUserRepository.java
@@ -0,0 +1,11 @@
+package com.rebekajakob.library.repository;
+
+import com.rebekajakob.library.model.LibraryUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LibraryUserRepository extends JpaRepository<LibraryUser, UUID> {
+}
