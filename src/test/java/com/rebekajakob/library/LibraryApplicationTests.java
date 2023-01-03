6b404f4d4d93794e2c564b4357fa2bd73ef2b33e
rebekajakob <rebusacka@gmail.com>
2023-01-03 14:06:16 +0100

    starter project

---
+++ src/test/java/com/rebekajakob/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebekajakob.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
