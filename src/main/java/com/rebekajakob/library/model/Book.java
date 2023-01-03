7db01bbfec40ed5d00542bdd887dea6ca18eb491
rebekajakob <rebusacka@gmail.com>
2023-01-03 14:14:24 +0100

    add @Getter to Book

--- src/main/java/com/rebekajakob/library/model/Book.java
+++ src/main/java/com/rebekajakob/library/model/Book.java
@@ -1,10 +1,12 @@
 package com.rebekajakob.library.model;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @Builder
+@Getter
 public class Book {
     private String title;
     private Author author;
