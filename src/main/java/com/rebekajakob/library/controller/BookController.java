816cfdcfbb92f2bca620ffcd733ef1f26cf13280
rebekajakob <rebusacka@gmail.com>
2023-01-03 16:10:52 +0100

    implement addBook method in BookController

--- src/main/java/com/rebekajakob/library/controller/BookController.java
+++ src/main/java/com/rebekajakob/library/controller/BookController.java
@@ -1,11 +1,29 @@
 package com.rebekajakob.library.controller;
 
 
+import com.rebekajakob.library.model.Book;
+import com.rebekajakob.library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public void addBook(@RequestBody Book book){
+        bookService.addBook(book);
+    }
+
+
 
 }
