97693fb8571d8cb1784206730f82e2384ce81b77
rebekajakob <rebusacka@gmail.com>
2023-01-03 15:45:48 +0100

    create BookController class

---
+++ src/main/java/com/rebekajakob/library/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.rebekajakob.library.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+}
