0f0ed3804fda212a73d31ca1572e078b2df1e4c4
rebekajakob <rebusacka@gmail.com>
2023-01-03 16:10:34 +0100

    implement addBook method in BookService

--- src/main/java/com/rebekajakob/library/service/BookService.java
+++ src/main/java/com/rebekajakob/library/service/BookService.java
@@ -1,4 +1,20 @@
 package com.rebekajakob.library.service;
 
+import com.rebekajakob.library.model.Book;
+import com.rebekajakob.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(Book book){
+        bookRepository.save(book);
+    }
 }
