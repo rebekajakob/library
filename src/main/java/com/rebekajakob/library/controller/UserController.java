f24b8bd5467fb44960fbf67368ee374b3b33e023
rebekajakob <rebusacka@gmail.com>
2023-01-03 15:48:13 +0100

    create UserController and implement addUser method

---
+++ src/main/java/com/rebekajakob/library/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.rebekajakob.library.controller;
+
+import com.rebekajakob.library.model.LibraryUser;
+import com.rebekajakob.library.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void addUser(@RequestBody LibraryUser libraryUser){
+        userService.addUser(libraryUser);
+
+    }
+}
