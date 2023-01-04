a4a8159f0c4fb72254847b88b1f187bb1954b188
rebekajakob <rebusacka@gmail.com>
2023-01-04 10:21:22 +0100

    create ReservationRepository and implement getReservationsByReservedBy method

---
+++ src/main/java/com/rebekajakob/library/repository/ReservationRepository.java
@@ -0,0 +1,12 @@
+package com.rebekajakob.library.repository;
+
+import com.rebekajakob.library.model.LibraryUser;
+import com.rebekajakob.library.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+    List<Reservation> getReservationsByReservedBy(LibraryUser libraryUser);
+}
