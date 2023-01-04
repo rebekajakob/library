85bb97a21fa0f8090f99fa9293bc29f7cbfa7ec6
rebekajakob <rebusacka@gmail.com>
2023-01-04 09:46:01 +0100

    implement getAuthorById and updateAuthor methods

--- src/main/java/com/rebekajakob/library/controller/AuthorController.java
+++ src/main/java/com/rebekajakob/library/controller/AuthorController.java
@@ -1,6 +1,7 @@
 package com.rebekajakob.library.controller;
 
 import com.rebekajakob.library.model.Author;
+import com.rebekajakob.library.model.Book;
 import com.rebekajakob.library.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,16 @@ public class AuthorController {
     @PostMapping
     public void addAuthor(@RequestBody Author author){
         authorService.addAuthor(author);
+    }
+
+    @GetMapping("/{authorId}")
+    public Author getAuthorById(@PathVariable String authorId){
+        return authorService.getAuthorByID(authorId);
+    }
+
+    @PutMapping("/{authorId}")
+    public void updateAuthor(@PathVariable String authorId, @RequestBody Author author){
+        authorService.updateAuthor(authorId,author);
     }
 
 
--- src/main/java/com/rebekajakob/library/service/AuthorService.java
+++ src/main/java/com/rebekajakob/library/service/AuthorService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AuthorService {
@@ -23,5 +24,20 @@ public class AuthorService {
 
     public List<Author> getAllAuthors(){
         return authorRepository.findAll();
+    }
+
+    public Author getAuthorByID(String authorId){
+        return authorRepository.findById(UUID.fromString(authorId)).get();
+    }
+
+    public void updateAuthor(String authorId, Author author){
+        Author currentAuthor = authorRepository.findById(UUID.fromString(authorId)).get();
+        if(author.getBirthday() != null && !author.getBirthday().equals(currentAuthor.getBirthday())){
+            currentAuthor.setBirthday(author.getBirthday());
+        }
+        if(author.getName() != null && !author.getName().equals(currentAuthor.getName())){
+            currentAuthor.setName(author.getName());
+        }
+        authorRepository.save(currentAuthor);
     }
 }
